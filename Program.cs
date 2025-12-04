78e65abb79919fe58dd39741fa794be8a9f28cc9
mamdouh <mamdouh@gmail.com>
2025-12-04 10:43:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IceMeltingSimulation
+{
+    class Program
+    {
+        static int MeltIce(string input)
+        {
+            char[] s = input.ToCharArray();
+            int meltedTotal = 0;
+
+            while (true)
+            {
+                List<int> meltIndices = new List<int>();
+
+                for (int i = 0; i < s.Length - 3; i++)
+                {
+                    if (s[i] == '_' && s[i + 1] == '_' && s[i + 2] == '_' && s[i + 3] == 'I')
+                    {
+                        meltIndices.Add(i + 3);
+                        i += 2;
+                    }
+                }
+
+                if (meltIndices.Count == 0)
+                    break;
+
+                foreach (int idx in meltIndices)
+                {
+                    s[idx] = '_';
+                    meltedTotal++;
+                }
+            }
+
+            return meltedTotal;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example 1:");
+            Console.WriteLine(MeltIce("I___I_I"));  // Output: 2
+
+            Console.WriteLine("Example 2:");
+            Console.WriteLine(MeltIce("III"));  // Output: 0
+        }
+    }
+}
